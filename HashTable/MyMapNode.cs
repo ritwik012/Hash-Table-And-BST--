1ba85ed440af7b39ab5524552db39953cc467ed4
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-28 23:10:02 +0530

    [Rutwik]Added:BinarySearchTree File

--- HashTable/MyMapNode.cs
+++ HashTable/MyMapNode.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace HashDemo
+namespace HashTablesAndBST
 {
     public class MyMapNode<K, V>
     {
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 
-namespace HashDemo
+namespace HashTablesAndBST
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("Welcome to Hash Tables and BST Programs");
-                Console.WriteLine("Choose the program to be executed : \n 1. Sentence Frequency \n 2. Exit");
+                Console.WriteLine("Choose the program to be executed : \n 1. Sentence Frequency \n 4. Binary Search Tree Creation \n 5. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
+                        MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
                         hash.Add("0", "To");
                         hash.Add("1", "be");
                         hash.Add("2", "or");
@@ -30,7 +30,14 @@ namespace HashDemo
                         string hash4 = hash.Get("4");
                         Console.WriteLine("4th index Value : " + hash4);
                         break;
-                    case 2:
+                    case 4:
+                        Console.WriteLine("***** Binary Search Tree *****");
+                        BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+                        binarySearch.Insert(30);
+                        binarySearch.Insert(70);
+                        binarySearch.Display();
+                        break;
+                    case 5:
                         flag = false;
                         break;
                 }
